ee1bd0eef858432894d8a754318b278f36af28f7
OlamideShogbamu <olamideshogbamu@gmail.com>
2024-07-31 10:49:08 +0100

    ressolved circular inport issue

--- app.py
+++ app.py
@@ -1,35 +1,56 @@
 from flask import Flask
+import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_caching import Cache
-from microplan import microplan_bp_
-from askchat import askchat_bp
-from askchat import initialize_db
+
+# from askchat import initialize_db
+# from microplan import initialize_db
 
 db = SQLAlchemy()
 migrate = Migrate()
 cache = Cache()
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 def create_app():
-    #app = Flask(__name__)
+    app = Flask(__name__)
+    app.json.sort_keys = False
     app.config.from_object('config.Config')
 
     db.init_app(app)
     migrate.init_app(app, db)
     cache.init_app(app)
 
+    from microplan import microplan_bp_, initialize_db
+    from askchat import askchat_bp, initialize_db as initialize_askchat_db
+
+    app.register_blueprint(microplan_bp_, url_prefix='/microplan')
+    app.register_blueprint(askchat_bp, url_prefix='/askchat')
 
     with app.app_context():
-        initialize_db(db)
+        # Initialize the database for microplan
+        init_microplan_db = initialize_db(db)
+        results = init_microplan_db()
+        
+        # If you need to use the results globally in microplan.py
+        import microplan
+        microplan.df = pd.DataFrame(results)
+        app.extensions['microplan_df'] = microplan.df
+
+        # Initialize the database for askchat if needed
+        initialize_askchat_db(db)
+        #initialize_db(db)
 
     return app
 
+#app = create_app
+
 # Register blueprints with URL prefixes
-app.register_blueprint(microplan_bp_, url_prefix='/microplan')
-app.register_blueprint(askchat_bp, url_prefix='/askchat')
+# app.register_blueprint(microplan_bp_, url_prefix='/microplan')
+# app.register_blueprint(askchat_bp, url_prefix='/askchat')
 
 if __name__ == "__main__":
     app = create_app()
+    app.json.sort_keys = False
     app.run(debug=True, host="0.0.0.0", port=8000)
