babfc8cdd566fcf6b2c392777534a5002024bcfc
OlamideShogbamu <shogbamuolamide@gmail.com>
2024-12-10 18:46:21 +0100

    updated home birth modeling

--- homebirthmodel.py
+++ homebirthmodel.py
@@ -16,7 +16,7 @@ def predict_outcome():
     data = request.get_json()
 
     # Extract the features from the request data
-    set_name = int(data.get('set_name'))
+    set_name = data.get('set_name')
     last_birth_caesarean = int(data.get('last_birth_caesarean'))
     religion = int(data.get('religion'))
     num_living_children = int(data.get('num_living_children'))
